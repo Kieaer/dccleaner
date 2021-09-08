24865866d93c5ba625e8a765782f1b0bb936e88a
키에르 <44261958+Kieaer@users.noreply.github.com>
2021-09-08 18:44:38 +0900

    2021. 9. 8 업데이트

--- cleaner.js
+++ cleaner.js
@@ -29,20 +29,23 @@ async function start(id, pw) {
             await browser.close();
         }
     });
+    await page.setDefaultNavigationTimeout(0)
     await page.goto('https://www.dcinside.com/');
     await page.type("#user_id", id)
     await page.type("#pw", pw)
     await page.click("#login_ok")
     await page.waitForNavigation()
-
+    await page.waitForTimeout(15000)
+    
     await page.goto(`https://gallog.dcinside.com/${id}/posting`);
-    var delete_button = "#container > article > div > section > div > div > ul > li:nth-child(1) > div > div > button"
+    var delete_button = "#container > article > div > div.wrap_right > section > div.gallog_cont.postings > div > ul > li:nth-child(1) > div > div > button"
     while (await page.$(delete_button) !== null) {
         await page.click(delete_button)
         await page.waitForNavigation()
         await page.waitForTimeout(1000)
     }
-    delete_button = "#container > article > div > section > div > div > ul > li > div > div > button"
+    
+    delete_button = "#container > article > div > div.wrap_right > section > div.gallog_cont.comments > div > ul > li:nth-child(1) > div > div > button"
     await page.goto(`https://gallog.dcinside.com/${id}/comment`);
     while (await page.$(delete_button) !== null) {
         await page.click(delete_button)
